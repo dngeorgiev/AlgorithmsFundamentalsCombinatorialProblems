745e7555a9c44db16f306334c78e64d9930693de
Dani Nedelchev <dngeorgiev@abv.bg>
2022-01-14 14:57:40 +0200

    Fixed algorithm for permutations with repetitions and optimized it with swapping algorithm

--- generatePermutationsWithRepetition/Main.java
+++ generatePermutationsWithRepetition/Main.java
@@ -1,34 +1,44 @@
 package generatePermutationsWithRepetition;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static String[] elements;
-    public static String[] permutes;
-    public static boolean[] used;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         elements = scanner.nextLine().split("\\s+");
-        permutes = new String[elements.length];
-        used = new boolean[elements.length];
 
         permuteWithRepetition(0);
     }
 
     private static void permuteWithRepetition(int index) {
         if (index == elements.length) {
-            print();
+            print(elements);
         } else {
-            for (String element : elements) {
-                permutes[index] = element;
-                permuteWithRepetition(index + 1);
+            permuteWithRepetition(index + 1);
+            HashSet<String> swapped = new HashSet<>();
+            swapped.add(elements[index]);
+            for (int i = index + 1; i < elements.length; i++) {
+                if (!swapped.contains(elements[i])) {
+                    swap(elements, index, i);
+                    permuteWithRepetition(index + 1);
+                    swap(elements, index, i);
+                    swapped.add(elements[i]);
+                }
             }
         }
     }
 
-    private static void print() {
+    private static void swap(String[] elements, int firstIndex, int secondIndex) {
+        String temp = elements[firstIndex];
+        elements[firstIndex] = elements[secondIndex];
+        elements[secondIndex] = temp;
+    }
+
+    private static void print(String[] permutes) {
         System.out.println(String.join(" ", permutes));
     }
 }
